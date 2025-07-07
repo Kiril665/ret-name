a00d0c6c47aa064a89727763b7016e9985c7c2d3
Kiril665 <vrbkirill09@gmail.com>
2025-07-07 18:08:03 +0300

    my hom work

---
+++ progect.py
@@ -0,0 +1,9 @@
+number = "42"
+padded = number.zfill(5)
+print(padded)
+# Виведе: '00042'
+
+text = "Привіт, мене звати Кирило"
+result = text.partition("мене")
+print(result)
+# Виведе: ('Привіт, ', 'мене', ' звати Кирило')
