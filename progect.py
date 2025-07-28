975de8fd954dcae0dcfdbc9a4910452dcbeed7a7
Kiril665 <vrbkirill09@gmail.com>
2025-07-28 18:27:45 +0300

    ret

--- progect.py
+++ progect.py
@@ -1,24 +1,10 @@
-while True:
-    print("\n=== КАЛЬКУЛЯТОР ===")
-    a = float(input("Введіть перше число: "))
-    b = float(input("Введіть друге число: "))
-    op = input("Оберіть операцію (+, -, *, /): ")
+# main.py
 
-    if op == "+":
-        print("Результат:", a + b)
-    elif op == "-":
-        print("Результат:", a - b)
-    elif op == "*":
-        print("Результат:", a * b)
-    elif op == "/":
-        if b != 0:
-            print("Результат:", a / b)
-        else:
-            print("Помилка: ділення на нуль!")
-    else:
-        print("Невірна операція!")
+import random
+name = ['Кіріл','Кіра','Ілля','Дамір','Юля','Віка','Игарь']
 
-    cont = input("Бажаєте продовжити? (так/ні): ").lower()
-    if cont != "так":
-        print("Калькулятор завершив роботу.")
-        break
+def generate_name():
+    return random.choice(name)
+
+
+print("Випадкове ім'я:", generate_name())
